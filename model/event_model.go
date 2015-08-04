ca372da96164ec6b34c91880295852c4099178c3
Biswas, Seemanta (US - Mumbai) <sebiswas@DELOITTE.com>
2015-08-04 18:37:13 +0530

    model structure updated

--- model/event_model.go
+++ model/event_model.go
@@ -1,60 +1,27 @@
-package model
-
-type event struct {
-	id 				string
-	title 			string
-	created_by 		string
-	created_on 		time.Time
-	participants 	int
-	volunteers 		int
-	time 			time.Time
-	duration 		int
-	location 		location
-	image 			image
-}
-
-type location struct{
-	locality string
-	city string
-	coordinates coordinates
-}
-type coordinates struct{
-	latitude float
-	longitude float
-}
-type image struct{
-	image_url string
-}
+package event_model
 
 type (
 	// CCEvent contains information for single event.
 	CCEvent struct {
-	id 				string		`cc:"_id notnull"`
-	title 			string		`cc:"wind_speed_milehour"`
-	created_by 		string		`cc:"wind_speed_milehour"`
-	created_on 		time.time 	`cc:"wind_speed_milehour"`
-	participants 	int 		`cc:"wind_speed_milehour"`
-	volunteers 		int 		`cc:"wind_speed_milehour"`
-	time 			time.Time 	`cc:"wind_speed_milehour"`
-	duration 		int 		`cc:"wind_speed_milehour"`
-	location 		location 	`cc:"wind_speed_milehour"`
-	image 			image 		`cc:"wind_speed_milehour"`
+		id 				string		`cc:"_id notnull"`
+		title 			string		`cc:"title"`
+		created_by 		string		`cc:"created_by"`
+		created_on 		time.time 	`cc:"created_on"`
+		participants 	int 		`cc:"participants"`
+		volunteers 		int 		`cc:"volunteers"`
+		time 			time.Time 	`cc:"time"`
+		duration 		int 		`cc:"duration_in_mins"`
+		location 		CCLocation 	`cc:"location"`
+		image 			CCImage 	`cc:"image"`
 		
 	}
 
-	// BuoyLocation contains the buoy's location.
-	BuoyLocation struct {
-		Type        string    `cc:"type"`
-		Coordinates []float64 `cc:"coordinates"`
+	CCImage struct{
+		image_url 		string		`cc:"image_url"`
 	}
-
-	// BuoyStation contains information for an individual station.
-	BuoyStation struct {
-		ID        cc.ObjectId `cc:"_id,omitempty"`
-		StationId string        `cc:"station_id"`
-		Name      string        `cc:"name"`
-		LocDesc   string        `cc:"location_desc"`
-		Condition BuoyCondition `cc:"condition"`
-		Location  BuoyLocation  `cc:"location"`
+	CCLocation struct{
+		locality 		string		`cc:"locality"`
+		city 			string		`cc:"city"`
+		coordinates 	[]float64 	`cc:"coordinates"`
 	}
 )
--- model/program_model.go
+++ model/program_model.go
@@ -1,13 +1,27 @@
-package model
+package program_model
 
-type program struct{
-  id            string
-  creation_date time.Time
-  title         string
-  thread_name   string
-  volunteers    int
-  genre         string
-  location      location
-  image         image
-  description   string
-}
+
+
+type (
+    // CCProgram contains information for single program.
+    CCProgram struct {
+      id              string      `cc:"_id notnull"`
+      creation_date   time.time   `cc:"creation_date"`
+      title           string      `cc:"title"`
+      thread_name     string      `cc:"thread_name"`
+      volunteers      int         `cc:"volunteers"`
+      genre           string      `cc:"genre"`
+      location        CCLocation  `cc:"location"`
+      image           CCImage     `cc:"image"`
+      description     string      `cc:"description"`
+    }
+    
+    CCImage struct{
+      image_url       string      `cc:"image_url"`
+    }
+    CCLocation struct{
+      locality        string      `cc:"locality"`
+      city            string      `cc:"city"`
+      coordinates     []float64   `cc:"coordinates"`
+    }
+)
